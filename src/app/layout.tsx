534417f52d4c572a7e2611dff3b85b187ff5b3ae
Kyle Coffelt <krcoffelt@gmail.com>
2026-04-30 17:10:51 -0500

    Install Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
@@ -7,6 +8,8 @@ import { AnalyticsScripts } from "@/components/AnalyticsScripts";
 import { StructuredData } from "@/components/StructuredData";
 import { companyInfo } from "@/data/company";
 import { buildLocalBusinessSchema, buildOrganizationSchema, buildWebsiteSchema } from "@/lib/seo";
+
+const googleTagManagerId = "GTM-NZ7X9N58";
 
 export const metadata: Metadata = {
   title: companyInfo.name,
@@ -50,7 +53,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${googleTagManagerId}');
+          `}
+        </Script>
+      </head>
       <body className="font-body antialiased">
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <StructuredData
           id="site-structured-data"
           data={[buildOrganizationSchema(), buildLocalBusinessSchema(), buildWebsiteSchema()]}
